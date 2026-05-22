f9124b70b1083d310b9dc85a5ed1281714060463
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-22 18:23:30 +0600

    Added: ICampaign Repositories

---
+++ EcoAct.Application/Services/Interfaces/ICampaignService.cs
@@ -0,0 +1,14 @@
+﻿using EcoAct.Application.DTOs.Campaigns;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoAct.Application.Services.Interfaces
+{
+    public interface ICampaignService
+    {
+        Task<IEnumerable<CampaignResponseDto>> GetAllCampaignsAsync();
+        Task<CampaignResponseDto> GetCampaignByIdAsync(Guid id);
+        Task<CampaignResponseDto> CreateCampaignAsync(CreateCampaignDto dto);
+    }
+}
