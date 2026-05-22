cac157676fb37b2db6d8555c2e1277a46ecf1280
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-22 13:01:04 +0600

    Added Camplaign Model

---
+++ EcoAct.Domain/Entities/Campaign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoAct.Domain.Entities
+{
+    internal class Campaign
+    {
+        public Guid id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Despcription { get; set; } = string.Empty;
+        public int TargetedTree { get; set; }
+        public int PlantedTree { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
