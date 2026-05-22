76a27b263fd03204a79372e44720c1c64f607b06
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-22 22:28:23 +0600

    Added: ApplictionDbContext

---
+++ EcoAct.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EcoAct.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoAct.Infrastructure.Data
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Campaign> Campaigns { get; set; }
+
+    }
+}
