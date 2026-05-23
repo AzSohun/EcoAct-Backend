1544cea94723c8d6dc4e6e2064bd04eb16ce93d3
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-23 14:34:23 +0600

    Added: campaign.ts

---
+++ frontend/src/app/core/campaign.ts
@@ -0,0 +1,15 @@
+
+
+
+export interface Campaign {
+
+    Id: string,
+    Title: string,
+    Description: string,
+    TargetTrees: number,
+    PlantedTrees: number,
+    StartDate: string,
+    EndDate: string,
+    IsActive: boolean,
+}
+
