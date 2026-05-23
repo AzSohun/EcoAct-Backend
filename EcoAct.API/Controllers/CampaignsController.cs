83ab8d710775e5ab8acba6cd305a0a5d975bc194
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-23 10:44:50 +0600

    Added: Campaigns Controller

---
+++ EcoAct.API/Controllers/CampaignsController.cs
@@ -0,0 +1,50 @@
+﻿using EcoAct.Application.DTOs.Campaigns;
+using EcoAct.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcoAct.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampaignsController : ControllerBase
+    {
+
+        private readonly ICampaignService _campaignService;
+
+        public CampaignsController(ICampaignService campaignService)
+        {
+            _campaignService = campaignService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCampaign([FromBody] CreateCampaignDto request)
+        {
+            var response = await _campaignService.CreateCampaignAsync(request);
+
+            return CreatedAtAction(nameof(GetByIdCampaign), new {id = response.Id}, response);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCampaign()
+        {
+            var campaigns = await _campaignService.GetAllCampaignsAsync();
+
+            return Ok(campaigns);
+        }
+
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetByIdCampaign(Guid id)
+        {
+            var campaign = await _campaignService.GetCampaignByIdAsync(id);
+
+            if(campaign == null)
+            {
+                return NotFound(new { Message =$"Campaign with this {id} not found." });
+            }
+
+            return Ok(campaign);
+        }
+    }
+}
---
+++ EcoAct.Infrastructure/Migrations/20260522203219_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoAct.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campaigns",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    TargetTrees = table.Column<int>(type: "integer", nullable: false),
+                    PlantedTrees = table.Column<int>(type: "integer", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campaigns", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Campaigns");
+        }
+    }
+}
